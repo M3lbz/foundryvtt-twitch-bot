933b4b8526d444177244bd29dd29cf12e097fb9b
Melbz <a.mellor91@gmail.com>
2020-12-28 15:38:24 +0000

    Added GM Whisper

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
-
 Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins it's initialization workflow.");
+  console.log("sdfsdfThis code runs once the Foundry VTT software begins it's initialization workflow.");
 });
 
 Hooks.on("ready", function() {
   console.log("This code runs once core initialization is ready and game data is available.");
 });
+
+
+function Whisper() {
+  ChatMessage.create({
+    content: "This is a test",
+    whisper: [game.users.find((u) => u.isGM)],
+    speaker: ChatMessage.getSpeaker({ alias: "Twitch" })
+  });
+} 
