69e6042b7c56028c8c18fc1e027ec6206a41e22d
Melbz <a.mellor91@gmail.com>
2020-12-28 18:00:00 +0000

    Added tmi

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,35 @@
 Hooks.on("init", function() {
-  console.log("sdfsdfThis code runs once the Foundry VTT software begins it's initialization workflow.");
+  game.settings.register("foundry-twitch-bot", "twitchBotChannelNames", {
+    name: "Player Channel Names",
+    hint: "Comma delimited list of channels you would like to monitor the chat for. e.g. \"channel1,channel2\"",
+    scope: "world",
+    config: true,
+    type: String,
+    default: ""
+  });
+
+  
 });
 
 Hooks.on("ready", function() {
-  console.log("This code runs once core initialization is ready and game data is available.");
+  game.twitchClient = new tmi.Client({
+    connection: {
+      secure: true,
+      reconnect: true
+    },
+    channels: game.settings.get('foundry-twitch-bot', 'twitchBotChannelNames').split(',').map((c) => c.trim())
+  });  
+
+  game.twitchClient.connect().catch(console.error);
+  game.twitchClient.on('message', (channel, tags, message, self) => {
+    WhisperGM(`<b>${tags['display-name']}</b>: ${message} (${channel})`);
+  });
 });
 
 
-function Whisper() {
+function WhisperGM(content) {
   ChatMessage.create({
-    content: "This is a test",
+    content: content,
     whisper: [game.users.find((u) => u.isGM)],
     speaker: ChatMessage.getSpeaker({ alias: "Twitch" })
   });
